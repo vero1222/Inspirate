2b6a346fbff2842bfb8437340a880780174d60e0
vero1222 <67570513+vero1222@users.noreply.github.com>
2020-06-28 21:44:42 -0600

    Add files via upload

---
+++ javasi.js
@@ -0,0 +1,12 @@
+function procesar(){
+	electronico=document.getElementById("correo").value;
+	sms=document.getElementById('mensaje').value
+	
+	if (electronico==''){
+		alert('Requisitos vacios');
+	}else(
+		alert('Email*'+electronico+'Mensaje'+sms)
+
+		)
+          
+}
